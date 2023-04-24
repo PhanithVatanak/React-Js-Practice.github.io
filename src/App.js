3f1fbb0a32dca45a4d6c890504d373b2b94d0da0
PhanithVatanak <phanith.vatanak@gmail.com>
2023-04-24 09:43:33 +0700

    add

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const expenses = [
+  { title : 'Macbook Air M1', amount : 1045.56, date: new Date(2021, 11, 14)},
+  { title : 'Macbook Air M2', amount : 1145.49, date: new Date(2022, 3, 5)},
+  { title : 'Macbook Pro M1', amount : 1025.37, date: new Date(2022, 11, 14)},
+  { title : 'Macbook Pro M2', amount : 1245.56, date: new Date(2023, 7, 1)},
+  { title : 'Toyota Camry 07 Hybrid', amount : 18600, date: new Date(2023, 4, 19)},
+]
+
+function App() {
+
+  const addExpenseHandler = ((expense) => {
+    console.log('IN App.js');
+    console.log(expense)
+  })
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
